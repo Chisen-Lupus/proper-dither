56a21990cd6f813eae5595691015fd93c04b758a
Yichen Liu @ EREBUS <yichenliu@arizona.edu>
2025-03-15 17:55:48 -0700

    migrated final product to process.py and restored previous version

--- dither/updated.py
+++ dither/updated.py
@@ -198,6 +198,8 @@ def real2dfft_backward(data):
     return data_hat
 
 
+### NOTE: TESTING CODE BUT CAN USE FOR 2X2 DITHER SAFELY
+
 PI = np.pi
 RADIOAN = PI/180
 NSUB = 2
@@ -232,7 +234,6 @@ def phase(A_in, offsets, npos):
     vec = np.zeros((NDIV**2, NDIV**2), dtype=np.complex128)
     coef = np.zeros((NDIV**2), dtype=np.complex128)
     phases = np.zeros((NDIV**2, NST), dtype=np.complex128)
-    key = np.zeros(NDIV**2)
     
     # LINE 247 - read offsets (totally different from the original code)
 
@@ -364,6 +365,8 @@ def phase(A_in, offsets, npos):
 
     return A
 
+# NOTE: same as read2dfft_* but used np.fft.fft2 to accelerate
+
 N514 = NC_FREQ
 N512 = NR_FREQ
 N257 = N514//2
@@ -417,180 +420,46 @@ def legacy_ifft2(A):
     data_return[:N512] = data_rec.real
     return data_return
 
-def combine_image(normalized_atlas, centroids, wt, oversample=2):
+from .wrapper import NR_FREQ, NC_FREQ
+from .wrapper import set_padding_size
+NSUB = 2
 
-    # SOME GLOBAL FACTORS
+NC, NR = 2**9+2, 2**9
+set_padding_size(NC, NR)
 
-    NSUB = oversample
-    NPP = len(normalized_atlas)
-    NX, NY = normalized_atlas[0].shape
-    NX_LARGE = NX*NSUB
-    NY_LARGE = NY*NSUB
-    N =int(np.ceil(np.log2(np.max([NX_LARGE, NY_LARGE]))))
-    NC_FREQ = 2**N + 2
-    NR_FREQ = 2**N
+# NOTE: THIS IS THE ONLY FUNCTION MIGHT BE DIRECTLY USED IN APPLICATION
 
-    Atotal = np.zeros((NC_FREQ//2, NR_FREQ), dtype=np.complex128)
+def phase_shift(A, offsets, n, verbose=False):
+    """
+    Apply phase shifts to the input data.
 
-    for npos in range(len(normalized_atlas)): 
+    Parameters
+    ----------
+    A : ndarray
+        Input 2D array representing Fourier-transformed data.
+    offsets : list of lists
+        Array of offsets and weights, with shape (n, 3), where the columns are
+        x-offset, y-offset, and weight.
+    n : int
+        Index of the position for which the coefficients are computed.
+    verbose : bool, optional
+        Whether to suppress internal print statements. Default is False.
 
-        # BEGIN PHASE
-
-        data = normalized_atlas[npos]
-        data_large = np.zeros((NR_FREQ, NR_FREQ))
-        data_large[:NX*NSUB:NSUB, :NY*NSUB:NSUB] = data
-        coef = np.zeros((NSUB**2), dtype=np.complex128)
-        
-        # LINE 247 - read offsets (totally different from the original code)
-
-        dx = centroids[:, 1]
-        dy = centroids[:, 0]
-        phix = NSUB*np.pi*dx
-        phiy = NSUB*np.pi*dy
-
-        # LINE 289 - Calculate the coefficients for each image. 
-
-        nsy = NSUB # 2
-        nsx = (NSUB - 1)//2 + 1 # 1
-        isy = -((NSUB-1)//2) + 1 # 0, NOTE: added a bracket to regulate different handling of integer division
-        isy = 0
-        isec = 0
-
-        # print('iy', list(range(isy, isy+nsy)))
-        # print('ix', list(range(0, nsx)))
-        for iy in range(isy, isy+nsy): 
-            for ix in range(0, nsx): 
-
-                # LINE 304
-
-                # Precompute normalized phase shifts
-                px = -2 * phix / NSUB
-                py = -2 * phiy / NSUB
-
-                # Compute base indices and initial phases
-                nuin = ix - (NSUB - 1) // 2
-                nvin = iy
-                pxi = nuin * px
-                pyi = -nvin * py
-
-                # Generate sub-grid indices
-                isatx, isaty = np.meshgrid(np.arange(NSUB), np.arange(NSUB))
-                isatx = isatx.flatten()
-                isaty = isaty.flatten()
-
-                # Calculate total phase using broadcasting
-                phit = np.outer(isatx, px) + pxi + np.outer(isaty, py) + pyi
-
-                # Compute complex phases and normalize
-                phases = (np.cos(phit) + 1j * np.sin(phit)) / NSUB**2
-
-                # Pivot the fundamental component to the first row
-                nfund = NSUB * nvin - nuin
-                phases[[0, nfund], :] = phases[[nfund, 0], :]
-
-                # LINE 355 - This loads an identity matrix, which will be used to invert the phase matrix.
-
-                # LINE 371 - The weighting factor is used at this point.
-
-                if NPP>NSUB**2: 
-                    phasem = phases @ np.diag(wt) @ np.conj(phases).T
-                else: 
-                    phasem = phases
-
-                vec = np.linalg.inv(phasem)
-
-                # LINE 490 - For NSUB2 images, we are done
-
-                if NPP==NSUB**2:
-                    coef[isec] = vec[npos, 0]
-
-                # LINE 495 - Otherwise, we need to do a little more work. Here we just solve for the fundamental image.
-
-                else: 
-                    coef[isec] = 0
-                    for i in range(NSUB**2):
-                        coef[isec] += vec[i, 0]*np.conj(phases[i, npos])
-
-                # LINE 505 - Addin weighting factor
-
-                coef[isec] *= wt[npos]
-
-                # print(f'Image {npos}, power {coef[isec]*np.conj(coef[isec])}, sector {isec}')
-                # print(f'Image {npos}, power {coef[isec]}, sector {isec}')
-
-                isec += 1
-
-        # LINE 516 - apply the complex scale factor to the transform
-        
-        # BEGIN FFT2
-
-        A_hat = scipy.fft.fft2(data_large) # data_large must be (2^N, 2^N) for now
-        A_unique = A_hat[:NC_FREQ//2, :]  # shape (NC_FREQ//2, NR_FREQ)
-        A_complex = np.conj(A_unique)
-
-        # END FFT2
-
-        isec = 0
-        isv = NR_FREQ//2
-        iev = isv - NR_FREQ//NSUB + 1 
-        for iy in range(isy, isy+nsy):
-            ieu = NC_FREQ - (NSUB - 1)*(nsx - 1)*NC_FREQ//NSUB
-            # print(ieu) # TODO: verify that ieu==NC_FREQ if nsx = 1
-            isu = 1
-            for ix in range(0, nsx):
-                isec += 1
-
-                # Extract the complex coefficient
-                coef_complex = coef[isec-1]
-
-                # Compute the normalized row positions (V)
-                # print(isv, iev-1)
-                rows = np.arange(isv, iev-1, -1)
-                rows = np.where(rows > 0, rows, NR_FREQ + rows)
-                V = np.where(rows > NR_FREQ // 2, (rows - NR_FREQ - 1) / NR_FREQ, (rows - 1) / NR_FREQ)
-
-                # Compute the row phase shift (as a complex exponential)
-                rphase = np.exp(-2j * phiy[npos] * V)
-
-                # Compute the normalized column positions (U)
-                cols = np.arange(isu, ieu + 1, 2)
-                U = (cols - 1) / (NC_FREQ - 2) / 2
-
-                # Compute the column phase shift (as a complex exponential)
-                cphase = np.exp(-2j * phix[npos] * U)
-
-                # Compute the overall phase shift (outer product for broadcasting)
-                phase_shift = coef_complex * np.outer(cphase, rphase)
-
-                # Apply the phase shift to A
-                A_complex[np.ix_(cols // 2, rows - 1)] *= phase_shift
-                # print('rows', (cols // 2)[[0, -1]], 'cols', (rows - 1)[[0, -1]])
-                
-                isu = ieu + 1
-                ieu = NC_FREQ - (nsx - 2 - ix)*NC_FREQ//NSUB # TODO: check values
-                # print(isu, ieu)
-            isv = iev - 1
-            iev = isv - NR_FREQ//NSUB + 1 # TODO: check values
-            if iy==(isy + nsy - 2): 
-                iev = -(NR_FREQ//2) + 1 # NOTE: add a bracket to change negative sign to minus sign
-
-        Atotal += np.conj(A_complex)
-        
-        print('---')
-    
-    # END PHASE
-
-    # BEGIN IFFT2
-
-    F = np.zeros((NR_FREQ, NR_FREQ), dtype=np.complex128)
-    F[:NC_FREQ//2, :] = Atotal
-    F[NC_FREQ//2:, 0] = np.conj(Atotal[1:NR_FREQ//2])[::-1, 0]
-    F[NC_FREQ//2:, 1:] = np.conj(Atotal[1:NR_FREQ//2])[::-1, :0:-1]
-    data_rec = scipy.fft.ifft2(F)
-    data_real = data_rec.real
-
-    # END IFFT2
-
-    combined_image = data_real[:NX_LARGE, :NY_LARGE]
-
-    return combined_image
+    Returns
+    -------
+    ndarray
+        Modified 2D array after phase adjustment.
+    """
+    if not verbose: 
+        stdout_backup = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+        try: 
+            Aphased = phase(A, offsets, n)
+        except Exception as e: 
+            sys.stdout = stdout_backup
+            print(repr(e))
+        finally:
+            sys.stdout = stdout_backup
+    else: 
+        Aphased = phase(A, offsets, n)
+    return Aphased
